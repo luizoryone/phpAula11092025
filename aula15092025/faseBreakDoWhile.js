367ed7c8d152b1dc7135bc2ffe0f06137dba3a9f
luizoryone <luizoryone@gmail.com>
2025-09-15 11:28:31 -0300

    Add loop with break condition for fase progression
    - Implemented a for loop that iterates through phases 1 to 10.
    - Added a console log to display the current phase.
    - Introduced a break condition to exit the loop when fase equals 5, with a message indicating the exit.

---
+++ aula15092025/faseBreakDoWhile.js
@@ -0,0 +1,7 @@
+for (let fase =1; fase <=10; fase++){
+    console.log(`Fase: ${fase}`);
+    if(fase === 5){
+        console.log("Fase 5 alcançada, saindo do loop."+fase);
+        break
+    }
+}
